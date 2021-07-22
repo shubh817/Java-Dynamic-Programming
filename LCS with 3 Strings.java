23ab24bec9903e42999e150da331a78b9b83f406
shubh817 <59890154+shubh817@users.noreply.github.com>
2021-07-22 08:47:52 +0530

    Update LCS with 3 Strings.java

--- LCS with 3 Strings.java
+++ LCS with 3 Strings.java
@@ -1,3 +1,24 @@
+/*
+Given 3 strings ,the task is to find the longest common sub-sequence in all three given sequences.
+
+Input Format
+First line contains first string . Second line contains second string. Third line contains the third string.
+
+Constraints
+The length of all strings is |s|< 200
+
+Output Format
+Output an integer denoting the length of longest common subsequence of above three strings.
+
+Sample Input
+GHQWNV
+SJNSDGH
+CPGMAH
+Sample Output
+2
+Explanation
+"GH" is the longest common subsequence
+*/
 import java.util.*;
 public class Main{
 
